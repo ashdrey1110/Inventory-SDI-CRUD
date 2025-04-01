da51535c7203c0e81d72038e00555f1740dd244f
Ashley Reynolds <ashrey1110@Ashleys-MacBook-Pro.local>
2025-04-01 15:16:53 -0700

    fixed component imports

---
+++ client/src/components/ItemDetails.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom';
+
+export default function ItemDetails() {
+
+    return (
+        <>
+        <div>Hello !</div>
+        </>
+    )
+}
