8f66cc581ad7e75a6c91e0593fc572c67b5f9c6b
Ashley Reynolds <ashrey1110@Ashleys-MacBook-Pro.local>
2025-04-01 12:57:59 -0700

    full server get, post, patch, del fxn complete

--- server/src/app.js
+++ server/src/app.js
@@ -132,3 +132,100 @@ app.get('/fullitems', (req, res) => {
 CREATE - POST
 
 */
+app.post('/users', (req, res) => {
+    const { firstName, lastName, username, password } = req.body;
+    knex('users')
+        .insert({ firstName, lastName, username, password })
+        .returning('id')
+        .then(() => res.status(201).json({message: `User added successfully`}))
+        .catch(err => 
+            res.status(500).json({
+                message: 'User could not be added'
+            })
+        );
+})
+
+app.post('/items', (req, res) => {
+    const { userId, itemName, description, quantity } = req.body;
+    knex('items')
+        .insert({ userId, itemName, description, quantity })
+        .returning('id')
+        .then(() => res.status(201).json({message: `Item added successfully`}))
+        .catch(err => 
+            res.status(500).json({
+                message: 'Item could not be added'
+            })
+        );
+})
+
+
+
+/*
+
+UPDATE - PATCH
+
+*/
+// app.patch('/users/:id', (req, res) => {
+//     let id = req.params.id;
+//     const { firstName, lastName, username, password } = req.body;
+//     knex('users')
+//         .where("id", id)
+//         .update({ firstName, lastName, username, password })
+//         .then((count) => {
+//             if(count >0){
+//                 res.status(200).json({message: `User info updated successfully`});
+//             }
+//             else{
+//                 res.status(404).json({error: "User not found"});
+//             }
+//         })
+//         .catch(err => 
+//             res.status(500).json({
+//                 message: 'User could not be updated'
+//             })
+//         );
+// })
+
+app.patch('/items/:id', (req, res) => {
+    let id = req.params.id;
+    const { userId, itemName, description, quantity } = req.body;
+    knex('items')
+        .where("id", id)
+        .update({ userId, itemName, description, quantity })
+        .then((count) => {
+            if(count >0){
+                res.status(200).json({message: `Item info updated successfully`});
+            }
+            else{
+                res.status(404).json({error: "Item not found"});
+            }
+        })
+        .catch(err => 
+            res.status(500).json({
+                message: 'Item could not be updated'
+            })
+        );
+})
+
+
+/*
+
+DELETE - DELETE
+
+*/
+
+app.delete('/items/:id', (req, res) => {
+    const { id } = req.params;
+    knex('items')
+        .where({ id }).del()
+        .then(() => {
+            res.status(201).json({
+                message: "Item removed successfully!"
+            })
+        })
+        .catch(err => 
+            res.status(500).json({
+                message: 'Item could not be removed'
+            })
+        );
+})
