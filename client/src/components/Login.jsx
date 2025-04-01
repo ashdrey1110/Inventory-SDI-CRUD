3a7b9c7afa2c1d1207c09018dfdfea2f164372d5
Ashley Reynolds <ashrey1110@Ashleys-MacBook-Pro.local>
2025-04-01 15:13:56 -0700

    added init frontend

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom';
+
+export default function Login() {
+
+    return (
+        <>
+        
+        </>
+    )
+}
